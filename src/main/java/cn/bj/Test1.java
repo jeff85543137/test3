dc3f296daff3268c49ec722e3aed89279512d576
jeff <120872344@qq.com>
2018-11-05 19:51:25 +0800

    ttt

---
+++ src/main/java/cn/bj/Test1.java
@@ -0,0 +1,9 @@
+package cn.bj;
+
+import javax.jws.WebService;
+
+@WebService
+public interface Test1 {
+
+	public String sayHello();
+}
---
+++ src/main/java/cn/bj/TestImpl.java
@@ -0,0 +1,10 @@
+package cn.bj;
+
+public class TestImpl implements Test1 {
+
+	@Override
+	public String sayHello() {
+		return "hello";
+	}
+
+}
